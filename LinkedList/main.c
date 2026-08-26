d22421c0b593e962a8a46b405b2ea9b498b0c7cb
v2fingers <sjmshelton@proton.me>
2026-08-27 11:35:22 +1200

    Two way linked list

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -4,6 +4,7 @@
 struct Node {
   int data;
   struct Node *next;
+  struct Node *back;
 };
 
 int main() {
@@ -22,17 +23,27 @@ int main() {
 
   node1->data = 13;
   node1->next = node2;
+  node1->back = NULL;
 
   node2->data = 100;
   node2->next = node3;
+  node2->back = node1;
 
   node3->data = 20;
   node3->next = NULL;
+  node3->back = node2;
 
   struct Node *temp = node1;
   while (temp != NULL) {
     printf("%d -> ", temp->data);
     temp = temp->next;
+  }
+  printf("END\n");
+
+  temp = node3;
+  while (temp != NULL) {
+    printf("%d -> ", temp->data);
+    temp = temp->back;
   }
   printf("END\n");
 
